437a1abd5b924e3673f505063697f2fe6c9d2486
Gowtham123ai <chgowthaman73@gmail.com>
2026-02-04 11:55:12 +0530

    Update api.js

--- FRONTEND/src/api.js
+++ FRONTEND/src/api.js
@@ -1,14 +1,12 @@
 import axios from "axios";
+const API_BASE = "https://financial-health-backend.onrender.com";
 
 export const uploadFile = (file, language, industry) => {
   const formData = new FormData();
   formData.append("file", file);
 
   return axios.post(
-    `http://127.0.0.1:8000/upload/?language=${language}&industry=${industry}`,
-    formData,
-    {
-      headers: { "Content-Type": "multipart/form-data" }
-    }
+    `${API_BASE}/upload/?language=${language}&industry=${industry}`,
+    formData
   );
 };
